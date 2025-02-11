2d53d508212a04070c5175f267d0eb69d22389fd
João Francisco Rodrigues <jfrodrigues42@gmail.com>
2025-02-11 14:24:59 -0300

    script links

---
+++ create_links.sh
@@ -0,0 +1,8 @@
+ln -s ~/.config/nvim ~/.dotfiles/nvim
+ln -s ~/.config/i3 ~/.dotfiles/i3
+ln -s ~/.config/polybar ~/.dotfiles/polybar
+ln -s ~/.zshrc ~/.dotfiles/.zshrc
+ln -s ~/.zsh ~/.dotfiles/.zsh
+ln -s ~/.gitconfig ~/.dotfiles/.gitconfig
+
+echo "Links criados"
